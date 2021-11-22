0935eaf6adb4398989b011c2086569462b8d776b
Student-Hussei2 <>
2021-11-22 14:09:20 +0000

    Initial commit

---
+++ Book.java
@@ -0,0 +1,79 @@
+public class Book
+
+{
+  // Fields/Instance Data
+  private String title;
+  private String author;
+  private int year;
+  private int numPages;
+  private boolean hardCover;
+
+  // Constructor
+  public Book(String t, String a, int y, int n, boolean h) {
+    title = t;
+    author = a;
+    year = y;
+    numPages = n;
+    hardCover = h;
+  }
+
+  // Accessor Methods
+public String getTitle(){
+  return title;
+}
+
+public String getAuthor(){
+  return author;
+}
+
+public int getYear(){
+  return year;
+}
+
+public int getNumPages(){
+  return numPages;
+}
+
+public boolean getHardCover(){
+  return hardCover;
+}
+
+//Mutator Methods - setter Methods
+public void setTitle(String s){
+title = s;
+}
+
+public void setAuthor(String s){
+author = s;
+}
+
+public void setYear(int s){
+year = s;
+}
+
+public void setNumPages(int s){
+numPages = s;
+}
+
+public void setHardCover(boolean s){
+hardCover = s;
+}
+
+//Other Methods
+public boolean equals(Book b){
+  if(this.getTitle().equals(b.getTitle())){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+  // What prints for our object
+  public String toString() {
+    String str = "";
+    str = "Title: " + title + "\nAuthor: " + author + "\nYear Published: " + year;
+    str = str + "\nNumber of Pages: " + numPages + "\nHard Cover: " + hardCover;
+    return str;
+  }
+
+}
---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+import java.lang.System.*;
+
+class Main {
+  public static void main(String[] args) {
+
+    Book b = new Book("I am Malala", "Malala Yousafzai", 2013, 288, true);
+
+    System.out.println(b);
+      
+    System.out.println();
+
+    Book b2 = new Book("The Cat in the Hat", "Dr. Seuss", 1957, 61, false);
+        
+    System.out.println(b2);
+
+    System.out.println();
+    System.out.println("Numpages of \"The Cat in the Hat\": " + b2.getNumPages());
+
+    System.out.println("Is \"" + b.getTitle() + "\" hardcover: " + b.getHardCover());
+
+    System.out.println();
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter new number of pages for " + b.getTitle() + ":");
+    int np = scan.nextInt();
+    b.setNumPages(np);
+    System.out.println(b);
+
+    System.out.println();
+    Scanner scan2 = new Scanner(System.in);
+    System.out.println("Change Author for " + b2.getTitle() + ":");
+    String a = scan2.nextLine();
+    b2.setAuthor(a);
+    System.out.println(b2);
+
+    System.out.println("Are the books equal? " + b.equals(b));
+
+    System.out.println("Are the books equal? " + b.equals(b2));
+
+  }
+}
